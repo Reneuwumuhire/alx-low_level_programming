0b97bb3949348ce2fe107514fc2c2b882677c816
Rene Uwumuhire <reneuwumuhire@gmail.com>
2023-03-31 14:27:00 -0700

    Changes things

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,30 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
+ * _strcat - concatenates two strings dest and src
  * @dest: string to append to
  * @src: string to add
  *
  * Return: a pointer to the resulting string
  */
-char *_strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int dest_len = 0;
-	while (dest[dest_len] != '\0')
-		dest_len++;
-	for (int src_len = 0; src[src_len] != '\0'; src_len++, dest_len++)
-		dest[dest_len] = src[src_len];
-	dest[dest_len] = '\0';
-       
-	return dest;
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
